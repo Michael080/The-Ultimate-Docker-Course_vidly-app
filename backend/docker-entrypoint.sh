35443aeb5233a2a7ca744d6b449b8a8ec0e6a7e1
Michael080 <mrollin808@gmail.com>
2022-04-04 18:36:18 -0600

    Setup database-migration tool 'migrate-mongo'

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
 echo "Waiting for MongoDB to start..."
 ./wait-for db:27017 
 
-echo "Migrating the databse..."
+echo "Migrating the database..."
 npm run db:up 
 
 echo "Starting the server..."
